bf7bccd90e64b7237e5a292d76bdacfd8cbbf17e
tabatahg <tabatahg@gmail.com>
2019-11-03 11:20:36 -0800

    hide x on load, disappears on click

--- JavaScript/mainsidebar.js
+++ JavaScript/mainsidebar.js
@@ -1,11 +1,15 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "300px";
-    document.getElementById("main").style.marginLeft = "200px";
+  document.getElementById("mySidebar").style.width = "300px";
+  document.getElementById("main").style.marginLeft = "200px";
+  const closeButton = (document.getElementById("closeButton").style.display =
+    "block");
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  const closeButton = (document.getElementById("closeButton").style.display =
+    "none");
 }
